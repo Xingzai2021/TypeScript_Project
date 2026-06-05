754a29305c88c8fee8d1b5efda7de1b922d9c93d
xingzai <785411429@qq.com>
2026-06-05 17:03:23 +0800

    first commit

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(message) {
+    return `Hello, ${message}!`;
+}
+console.log(greet("XingZai666"));
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function greet(message: string): string {
+    return `Hello, ${message}!`;
+}
+
+console.log(greet("XingZai666"));
